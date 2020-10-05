16c7da48ec9184de219b48dea9a650427e538dc3
Camilla Rygh <camillahallo@mail.com>
2020-10-05 16:56:27 +0200

    Change to function component and use useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 // The <App /> component is responsible for rendering the two main columns
-import React from "react";
+import React, { useState } from "react";
 import { BaseStyles } from "@primer/components";
 import { Flex } from "@primer/components";
 import PokemonList from "./columns/PokemonList";
 import PokemonDetails from "./columns/PokemonDetails";
 
-class App extends React.Component {
-  state = {
-    selectedPokemon: null
-  };
+export default function App() {
+  const [selectedPokemon, setSelectedPokemon] = useState(null);
 
-  setSelectedPokemon = name => {
-    this.setState({
-      selectedPokemon: name
-    });
-  };
+  /*   updateDocumentTitle() {
+    const { selectedPokemon } = this.state;
+    document.title = `${selectedPokemon ? `${selectedPokemon} | ` : ""}Pokedex`;
+  }
 
   componentDidMount() {
     this.updateDocumentTitle();
@@ -25,22 +22,14 @@ class App extends React.Component {
       this.updateDocumentTitle();
     }
   }
+ */
 
-  updateDocumentTitle() {
-    const { selectedPokemon } = this.state;
-    document.title = `${selectedPokemon ? `${selectedPokemon} | ` : ""}Pokedex`;
-  }
-
-  render() {
-    return (
-      <BaseStyles>
-        <Flex>
-          <PokemonList setSelectedPokemon={this.setSelectedPokemon} />
-          <PokemonDetails name={this.state.selectedPokemon} />
-        </Flex>
-      </BaseStyles>
-    );
-  }
+  return (
+    <BaseStyles>
+      <Flex>
+        <PokemonList setSelectedPokemon={setSelectedPokemon} />
+        <PokemonDetails name={selectedPokemon} />
+      </Flex>
+    </BaseStyles>
+  );
 }
-
-export default App;
